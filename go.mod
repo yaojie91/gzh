a173f7486e8ffd79029b7dc8d3a7c8bdae1c1848
yaojie <yaojie@ihealthlabs.com.cn>
2019-10-25 15:23:30 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,6 @@
+module https
+
+require (
+	github.com/gin-gonic/gin v1.4.0
+	github.com/labstack/echo v1.0.0
+)
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, "hello world")
+	})
+	router.Run(":11000")
+}
