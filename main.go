7bec3ac3335128c0d77dd71f0b082fe4935b9653
姚杰 <yaojie425@163.com>
2020-03-09 07:56:28 +0800

    删除无用代码

--- main.go
+++ main.go
@@ -11,20 +11,6 @@ func main() {
 	router := route.InitRouter()
 	endless.DefaultReadTimeOut = util.ReadTimeOut
 	endless.DefaultWriteTimeOut = util.WriteTimeOut
-	//router.GET("/hello", func(c *gin.Context) {
-	//	c.JSON(200, "hello world")
-	//})
-	//router.GET("/", func(c *gin.Context) {
-	//	signature := c.Query("signature")
-	//	timestamp := c.Query("timestamp")
-	//	nonce := c.Query("nonce")
-	//	echostr := c.Query("echostr")
-	//
-	//	sig := util.SignatureGen(timestamp, nonce)
-	//	if sig == signature {
-	//
-	//	}
-	//})
 
 	endless.ListenAndServe(":8080", router)
 }
